387b3e81fdfcb8326c2cef3199b93039501a0e72
nearworld <artemismars2@outlook.com>
2024-12-23 16:42:50 +0900

    feat: displaying table numbers

--- app/page.tsx
+++ app/page.tsx
@@ -12,29 +12,29 @@ const Page = () => {
             {/* 1 행: 1, 2 */}
             <div className='flex gap-4 md:gap-8 justify-between'>
               <div className='bg-gray-300 rounded-lg h-20 md:h-28 w-32 md:w-40 flex items-center justify-center'>
-                1
+                <span className='text-white font-bold text-2xl'>1</span>
               </div>
               <div className='bg-gray-300 rounded-lg h-20 md:h-28 w-32 md:w-40 flex items-center justify-center'>
-                2
+                <span className='text-white font-bold text-2xl'>2</span>
               </div>
             </div>
             {/* 2 행: 3, 4 */}
             <div className='flex gap-4 md:gap-8 justify-between'>
               <div className='bg-gray-300 rounded-lg h-20 md:h-28 w-32 md:w-40 flex items-center justify-center'>
-                3
+                <span className='text-white font-bold text-2xl'>3</span>
               </div>
               <div className='bg-gray-300 rounded-lg h-20 md:h-28 w-32 md:w-40 flex items-center justify-center'>
-                4
+                <span className='text-white font-bold text-2xl'>4</span>
               </div>
             </div>
             <div className='invisible'>empty</div>
             {/* 3 행: 5, 6 */}
             <div className='grid justify-items-center gap-1'>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-40 md:h-52 flex items-center justify-center'>
-                5
+                <span className='text-white font-bold text-2xl'>5</span>
               </div>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-40 md:h-52 flex items-center justify-center'>
-                6
+                <span className='text-white font-bold text-2xl'>6</span>
               </div>
             </div>
           </div>
@@ -44,29 +44,29 @@ const Page = () => {
             <div className='flex flex-col justify-end gap-4 md:gap-8'>
               <div className='flex gap-1 flex-col'>
                 <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-40 md:h-52 flex items-center justify-center'>
-                  7
+                  <span className='text-white font-bold text-2xl'>7</span>
                 </div>
                 <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-40 md:h-52 flex items-center justify-center'>
-                  8
+                  <span className='text-white font-bold text-2xl'>8</span>
                 </div>
               </div>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-40 md:h-52 flex items-center justify-center'>
-                9
+                <span className='text-white font-bold text-2xl'>9</span>
               </div>
             </div>
             {/* 2열: 10, 11, 12 */}
             <div className='flex flex-col gap-1 justify-end'>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-32 md:h-40 flex items-center justify-center'>
-                10
+                <span className='text-white font-bold text-2xl'>10</span>
               </div>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-32 md:h-40 flex items-center justify-center'>
-                11
+                <span className='text-white font-bold text-2xl'>11</span>
               </div>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-32 md:h-40 flex items-center justify-center'>
-                12
+                <span className='text-white font-bold text-2xl'>12</span>
               </div>
               <div className='bg-gray-300 rounded-lg w-24 md:w-32 h-32 md:h-40 flex items-center justify-center'>
-                13
+                <span className='text-white font-bold text-2xl'>13</span>
               </div>
             </div>
           </div>
@@ -74,10 +74,10 @@ const Page = () => {
           <div className='relative md:mt-10'>
             <div className='absolute bottom-20 md:bottom-32 flex gap-2'>
               <div className='bg-gray-300 rounded-lg w-40 md:w-52 h-24 md:h-32 flex items-center justify-center'>
-                14
+                <span className='text-white font-bold text-2xl'>14</span>
               </div>
               <div className='bg-gray-300 rounded-lg w-40 md:w-52 h-24 md:h-32 flex items-center justify-center'>
-                15
+                <span className='text-white font-bold text-2xl'>15</span>
               </div>
             </div>
           </div>
