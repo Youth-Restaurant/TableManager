45d35779602bf6da541dbfd3b05b13dbe7283533
nearworld <artemismars2@outlook.com>
2024-12-23 17:13:27 +0900

    refactor: use main element in main page and remove import React

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import React from 'react';
 import LeftSection from '@/components/LeftSection';
 import MiddleSection from '@/components/MiddleSection';
 import RightSection from '@/components/RightSection';
 
 const Page = () => {
   return (
-    <div className='min-h-screen w-11/12 md:w-full p-4'>
+    <main className='min-h-screen w-11/12 md:w-full p-4'>
       <div className='container mx-auto h-full'>
         <div className='flex flex-col md:flex-row gap-8 md:gap-16'>
           <LeftSection />
@@ -15,7 +14,7 @@ const Page = () => {
           <RightSection />
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
