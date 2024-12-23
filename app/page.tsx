d823c8b4a6841c791f683c8d06cf03d7f3f6a1f7
nearworld <artemismars2@outlook.com>
2024-12-23 16:24:00 +0900

    feat: basic table layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import React from 'react';
+
+const Page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='min-h-screen w-10/12 md:w-full p-4'>
+      <div className='container mx-auto h-full'>
+        <div className='flex gap-24'>
+          {/* 1 열: 좌측 홀 */}
+          <div className='flex flex-col gap-12 bg-white'>
+            {/* 1 행: 1, 2 */}
+            <div className='flex gap-8 justify-between'>
+              <div className='bg-gray-300 rounded-lg h-32 w-40'>1</div>
+              <div className='bg-gray-300 rounded-lg h-32 w-40'>2</div>
+            </div>
+            {/* 2 행: 3, 4 */}
+            <div className='flex gap-8 justify-between'>
+              <div className='bg-gray-300 rounded-lg h-32 w-40'>3</div>
+              <div className='bg-gray-300 rounded-lg h-32 w-40'>4</div>
+            </div>
+            <div className='invisible'>empty</div>
+            {/* 3 행: 5, 6 */}
+            <div className='grid justify-items-center gap-1'>
+              <div className='bg-gray-300 rounded-lg w-32 h-52'>5</div>
+              <div className='bg-gray-300 rounded-lg w-32 h-52'>6</div>
+            </div>
+          </div>
+          {/* 2 열: 중간 홀 */}
+          <div className='grid grid-cols-2 gap-24'>
+            {/* 1열: 7, 8, 9 */}
+            <div className='flex flex-col justify-end gap-8'>
+              <div className='flex gap-1 flex-col'>
+                <div className='bg-gray-300 rounded-lg w-32 h-52'>7</div>
+                <div className='bg-gray-300 rounded-lg w-32 h-52'>8</div>
+              </div>
+              <div className='bg-gray-300 rounded-lg w-32 h-52'>9</div>
+            </div>
+            {/* 2열: 10, 11, 12 */}
+            <div className='flex flex-col gap-1 justify-end'>
+              <div className='bg-gray-300 rounded-lg w-32 h-40'>10</div>
+              <div className='bg-gray-300 rounded-lg w-32 h-40'>11</div>
+              <div className='bg-gray-300 rounded-lg w-32 h-40'>12</div>
+              <div className='bg-gray-300 rounded-lg w-32 h-40'>13</div>
+            </div>
+          </div>
+          {/* 3 열: 우측 홀 */}
+          <div className='relative'>
+            <div className='absolute bottom-32 flex gap-2'>
+              <div className='bg-gray-300 rounded-lg w-52 h-32'>14</div>
+              <div className='bg-gray-300 rounded-lg w-52 h-32'>15</div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
